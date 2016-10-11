17fe68e0b197e5a9b5d7f08027c026f174e804e9
BeeMac4 <mureub@gmail.com>
2016-10-11 16:42:55 +0300

    Splitting words

---
+++ word count.py
@@ -0,0 +1,11 @@
+def words(phrase):
+	count_dict = {}
+	for l in phrase.split():
+		if l.isdigit():
+			count_dict.setdefault(int(l),0)
+			count_dict[int(l)] += 1
+		else:
+			count_dict.setdefault(l,0)
+			count_dict[l] += 1
+
+	return count_dict
